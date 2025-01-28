8669ab1a97b70d8adbbc151c7a179bb56ec9fda2
Certified Frontend/Backend/Mobile Developer, Project Manager <jamestaylor95425@gmail.com>
2025-01-28 09:23:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5009)  # Change port if needed
+    return "Hello World!"
