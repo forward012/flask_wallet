c515172712c1755494e19b70ac71e7a40f430d6f
Certified Frontend/Backend/Mobile Developer, Project Manager <jamestaylor95425@gmail.com>
2025-01-31 17:18:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello World!"
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
